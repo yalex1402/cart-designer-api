d447bdd973d588fc834aacf679a6701372cd313c
Alexander Garcia <yesidgarcialopez@gmail.com>
2022-01-21 11:34:23 -0500

    Adding contracts to Cart

--- ShoppingCartWs.Domain/Entities/Cart.cs
+++ ShoppingCartWs.Domain/Entities/Cart.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCartWS.Domain.Entities
 {
     public class Cart
     {
         public Guid Id { get; set; }
-        public double Total { get; set; }
-        public List<CartDetail> Details { get; set; }
+        public double Total => Items.Sum(cd => cd.Total);
+        public List<CartItem> Items { get; set; }
     }
 }
--- ShoppingCartWs.Domain/Repositories/ICartRepository.cs
+++ ShoppingCartWs.Domain/Repositories/ICartRepository.cs
@@ -1,10 +1,17 @@
+using System.Threading.Tasks;
 using ShoppingCartWS.Domain.Entities;
 
 namespace ShoppingCartWS.Domain.Repositories
 {
     public interface ICartRepository
     {
-        void AddToCart(Product product);
+        void AddItemToCart(CartItem cartItem);
+        Task AddItemToCartAsync(CartItem cartItem);
         void CleanCart(Cart cart);
+        Task CleanCartAsync(Cart cart);
+        void DeleteItemFromCart(CartItem cartItem);
+        Task DeleteItemFromCartAsync(CartItem cartItem);
+        void UpdateItemFromCart(CartItem cartItem);
+        Task UpdateItemFromCartAsync(CartItem cartItem);
     }
 }
