db03ea1c88f4306af004ff0baa62ad992380474b
Alexander Garcia <yesidgarcialopez@gmail.com>
2022-02-08 20:30:33 -0500

    Fixing unit test login with correct values

--- ShoppingCartWS.Tests/UserControllerTests.cs
+++ ShoppingCartWS.Tests/UserControllerTests.cs
@@ -115,6 +115,9 @@ namespace ShoppingCartWS.Tests
         [Fact]
         public async Task LoginAsync_WithCorrectValues_ReturnsOk()
         {
+            serviceManagerStub.Setup(serv => serv.TokenFactoryService.GenerateToken(It.IsAny<UserLoginDto>())).Returns(new TokenModel(It.IsAny<string>(),It.IsAny<DateTime>()));
+            userManagerStub.Setup(repo => repo.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(new IdentityUser());
             signInManagerStub.Setup(repo => repo.PasswordSignInAsync(It.IsAny<string>(),It.IsAny<string>(),false,false)).ReturnsAsync(Microsoft.AspNetCore.Identity.SignInResult.Success);
             var controller = new UserController(serviceManagerStub.Object,userManagerStub.Object,signInManagerStub.Object,roleManagerStub.Object);
             var result = await controller.LoginAsync(new LoginModel()
